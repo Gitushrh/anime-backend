0433cdef3348503db3fbe28a117e64c85f7ebb8c
Gitushrh <riflogame52@gmail.com>
2025-11-09 10:35:45 +0700

    update v6 base api

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
-const BASE_API = 'https://otakudesu-be-six.vercel.app';
+const BASE_API = 'https://otakudesu-be-six.vercel.app/';
 
 const httpsAgent = new https.Agent({
   rejectUnauthorized: false,
